74ca034ca5848b363cde6b3cbd2c720abed9882a
Charlie Doughertye <cedougherty@wisc.edu>
2025-06-30 15:22:49 -0500

    trying again

--- R/plotting_and_visualization.R
+++ R/plotting_and_visualization.R
@@ -11,12 +11,13 @@ setwd("~/Documents/R-Repositories/YaharaCl")
 yahara_spc = read_csv('data/Yahara_Buoys_master_20250527.csv') |> 
   mutate(year = year(date_time)#, 
          #depths = as.numeric(depths)
-         )
+         ) |> 
+  filter(spc > 200)
 
 # plot spc
 ggplot(yahara_spc, aes(date_time, spc, color = depths)) + 
   geom_path() + 
-  facet_wrap(vars(site)) +
+  facet_wrap(vars(site), scales = "free") +
   theme_bw()
 
 
